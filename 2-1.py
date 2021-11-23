2b0488b0092712cf9c1bdffa6375f09aadc94f4f
teimuri <teimurijervakani@gmail.com>
2021-11-23 04:51:30 -0800

    Test

---
+++ 2-1.py
@@ -0,0 +1,12 @@
+#Append
+first_list=['hami','96109996']
+first_list.append('taha')
+first_list.append('96110572')
+print(first_list)
+##############
+#List Of Lists
+second_list=['suny','222222']
+third_list=['sales','333333']
+list_of_list=[first_list,second_list,third_list]
+
+print(list_of_list[2][1])
---
+++ 2-3.py
@@ -0,0 +1,6 @@
+nation=['Roman','Egypt','Greek','Chinese','Islamic','Mayan','Persian','Mongol‬']
+golden_age=['27BC-1453AD','3150BC-30BC','800BC-600AD','221BC-1912AD','750AD-1257AD','2000BC-‬‬‫‪1540AD','550BC-651AD','1206AD-1368AD']
+
+world=zip(nation,golden_age)
+farhang_amin=dict(world)
+print(farhang_amin.get('Roman'))
