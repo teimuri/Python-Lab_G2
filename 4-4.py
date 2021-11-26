d0ab2be84a935a11c2290cdb54473d306ace8ed1
pythonlabtest <93882915+pythonlabtest@users.noreply.github.com>
2021-11-26 03:40:40 +0330

    Add files via upload

--- 4-4.py
+++ 4-4.py
@@ -1,23 +1,20 @@
-import string
-#List of English alphabets
-alphabet_list = list(string.ascii_uppercase)
-#26 zeros to make a dictionary of alphabets
-alphabet_count=[0]*26
-#Dictionary of alphabets
-dicte=dict(zip(alphabet_list,alphabet_count))
-#Set of available alphabets and converting them into a single string to count the number of availabe alphabets
-Table=[["ABCE"],["SFCS"],["ADEE"]]
-Table2= list(map(lambda x,y,z:x+y+z,Table[0],Table[1],Table[2]))
-#Counting and adding the number of available alphabets to the dictionary
-for j in Table2[0]:
-	dicte[j]=dicte[j]+1
+from iterator import iterator
+table=[["ABCE"],["SFCS"],["ADEE"]]
 
-a =input()
-#Subtrac number of needed alphabets from the available ones
-for j in a:
-	#print "False" if the number of an alphabet reaches 0 but it is stil needed
-	if dicte[j]==0:
-		print("False")
-		exit()
-	dicte[j]=dicte[j]-1
-print("True")
+row_lenght=len(table)
+coloumn_lenght=len(table[0][0])
+#Here in this 'for' loop every string is replaced by a series of charecters and placed inside 'new_table'
+#Take note that the 'row_lenght' and 'coloumn_lenght' will remain unchanged. 
+new_table=[]
+for i in table:
+	new_table.append(list(i[0]))
+
+The_input=input()
+answer=0	
+
+for selected_row in range(row_lenght):
+	for selected_coloumn in range(coloumn_lenght):
+		if new_table[selected_row][selected_coloumn]==The_input[0]:	#If there is a match with the fist charecter of input
+			answer=answer + iterator(new_table,[selected_row,selected_coloumn],The_input[1:])
+	
+print(answer>0)
