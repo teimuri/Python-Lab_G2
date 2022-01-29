d10f8e27616e69dd8ccf9e78c8c21280d4503db5
teimuri <teimurijervakani@gmail.com>
2022-01-29 04:16:59 -0800

    commented

--- LAB_6/sets.py
+++ LAB_6/sets.py
@@ -1,8 +1,21 @@
 import sys
+
+class set(object):
+	def __init__(self):
+		self.this_list=[]
+	def add(self,x):
+		if x not in self.this_list:
+			self.this_list.append(x)
+		
+	def delete(self,x):
+		if x in self.this_list:
+			self.this_list.remove(x)
+
+#Finding classed by their name in strig form
 def str_to_class(classname):
     return getattr(sys.modules[__name__], classname)
-    
-def duplicate_find(a,b,function):
+# 'do_operation' will... do the operation
+def do_operation(a,b,function):
 	union=set()
 	intersection=set()
 	for i in a.this_list:
@@ -19,11 +32,13 @@ def duplicate_find(a,b,function):
 		return(intersection)
 	elif function=='==':
 		return(len(intersection)==len(union))
-	
-def operation(command):
+
+# "operation_find" will find the operation required and the two sets being used.
+def operation_find(command):
 	first_set=""
 	second_set=""
 	operation=""
+	# "condition" detemines wether a or b is being read
 	condition=0
 	for i in command:
 		if i=='&' or i=='|' or i=='=':
@@ -33,34 +48,14 @@ def operation(command):
 			first_set=first_set+i
 		elif condition==1:
 			second_set=second_set+i
-	
-	(operation)
-	return duplicate_find(str_to_class(first_set),str_to_class(second_set),operation)
-class set(object):
-	def __init__(self):
-		self.this_list=[]
-	def add(self,x):
-		if x not in self.this_list:
-			self.this_list.append(x)
-		
-	def delete(self,x):
-		if x in self.this_list:
-			self.this_list.remove(x)
+	return do_operation(str_to_class(first_set),str_to_class(second_set),operation)
+
 	
 		
 a=set()
 b=set()
-a.add(3)
-a.add('hel')
-b.add('hel')
-b.add(2)
-b.add(2)
-f=a
-c=operation("a|b")
-c.add(7)
-f.add(85)
-f.delete('hel0')
-print(f.this_list)
-#print(a.this_list)
-#print(b.this_list)
-#print(operation("a|b"))
+a.add('hi')
+b.add('no a')
+c=operation_find('a|b')
+c=operation_find('a&b')
+print(c.this_list)
