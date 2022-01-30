ee1834e4a311fc33c73e952e086bb35e9794b2a4
teimuri <teimurijervakani@gmail.com>
2022-01-30 00:59:22 -0800

    all

--- LAB_2/1-2-1.sh
+++ LAB_2/1-2-1.sh
@@ -1,12 +1,21 @@
 #! /bin/bash
+#A directory is need for which we want to change the premissions
+
+#The address to code is stored in $code_address to keep runing it recursively
 code_address=$(pwd)
+
 cd $@
+
 for j in $(ls)
 do
-	chmod +sx $j
+	chmod a+rwx $j
 done
+
 directories=$(ls -d */)
 for i in $directories
 do
-	$code_address/test.sh $(pwd)/$i
+target_dir=$(pwd)
+cd $code_address
+./1-2-1.sh $target_dir/$i
+cd $target_dir
 done
